3111f28adb7a81c00a002bdbbebcd884c8147ec0
Elgedawy31 <mohamedelgedawy40@gmail.com>
2025-05-05 22:22:31 +0300

    save changes

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const index = () => {
+
+  return (
+    
+    <View>
+
+      <Text>index</Text>
+      
+    </View>
+  )
+
+}
+
+export default index
